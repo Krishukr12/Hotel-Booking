32f442f1e8eccc0c41d25e9aece7a57059a21f50
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2023-02-03 09:37:20 +0530

    Adding json token verification

---
+++ server/middlewares/verfifyToken.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+const createError = require("../utils/error.js");
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(createError(401, "You are not authenticated"));
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(createError(403, "Token is not valid"));
+    req.user = user;
+    next();
+  });
+};
