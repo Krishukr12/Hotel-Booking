2e39ac80b94a06cabb56bb33d4a8e98d3c6f1ff6
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2023-02-12 18:02:04 +0530

    Featured List page complete

---
+++ client/hotel_booking/src/Components/PropertyList/PropertyList.jsx
@@ -0,0 +1,56 @@
+import { ClassNames } from "@emotion/react";
+import React from "react";
+import classes from "./PropertyList.module.css";
+import { useState, useEffect } from "react";
+export const PropertyList = () => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    // setLoading(true);
+    callApi();
+  }, []);
+
+  const callApi = async () => {
+    fetch("http://localhost:8080/hotels/find/countByType", {
+      method: "GET", // or 'PUT'
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const images = [
+    "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
+  ];
+  return (
+    <div className={classes.pList}>
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          {data &&
+            images.map((img, i) => (
+              <div className={classes.pListItem} key={i}>
+                <img src={img} alt="" className={classes.pListImg} />
+                <div className={classes.pListTitles}>
+                  <h1>{data[i]?.type}</h1>
+                  <h2>
+                    {data[i]?.count} {data[i]?.type}
+                  </h2>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
+    </div>
+  );
+};
--- client/hotel_booking/src/Pages/Home/Home.jsx
+++ client/hotel_booking/src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { Navbar } from "../../Components/Navbar/Navbar";
 import Header from "../../Components/Header/Header";
 import classes from "./Home.module.css";
 import { Featured } from "../../Components/Featured/Featured.jsx";
+import { PropertyList } from "../../Components/PropertyList/PropertyList";
 
 export const Home = () => {
   return (
@@ -11,6 +12,8 @@ export const Home = () => {
       <Header />
       <div className={classes.homeContainer}>
         <Featured />
+        <h1 className={classes.homeTitle}>Browse by property type</h1>
+        <PropertyList />
       </div>
     </div>
   );
