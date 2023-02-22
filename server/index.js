7939920f22f2c5fe7eca3c7403fd83871149270a
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2023-02-22 14:25:28 +0530

    Changing port no

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const { usersRouter } = require("./routes/users.route.js");
 var cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => {
